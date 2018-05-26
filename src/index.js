663b9c46eb3b3216406ba6bee1ff459363c10bc2
Jace Mitsuka <PJace@BigMac.local>
2018-05-25 14:04:10 -1000

    installed redux, react-redux, and thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const boundCompose = compose.bind(null, applyMiddleware(thunk));
+const store = createStore(reducers);
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
